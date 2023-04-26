1bf8511719edf0299b30f7fafcbf37cd0ee23af5
JJAR140201 <avilesroajuanjose@gmail.com>
2023-04-26 08:05:58 -0500

    creacion backend prueba covinoc

---
+++ src/main/java/prueba/covinoc/PruebaCovinocApplication.java
@@ -0,0 +1,13 @@
+package prueba.covinoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCovinocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCovinocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/prueba/covinoc/PruebaCovinocApplicationTests.java
@@ -0,0 +1,13 @@
+package prueba.covinoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCovinocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
